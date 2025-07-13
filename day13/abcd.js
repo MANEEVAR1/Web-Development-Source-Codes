de44e99d3e57bfbb009f20d7be4e536d5c8143cf
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2025-07-13 17:00:28 +0530

    day13

---
+++ day13/abcd.js
@@ -0,0 +1,22 @@
+console.log("Hello world !!  --> from abcd.js file");
+
+// var
+var a = 20
+
+console.log(a)
+
+var a = 30
+
+console.log(a)
+
+// let a = 40;
+
+// console.log(a);
+
+// let a = 60;
+// console.log(a);
+
+
+const pi = 3.14
+
+console.log(pi)
